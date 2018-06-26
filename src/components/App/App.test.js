3c212f1f27a1034588583eeab304357df46a0222
Mickey MacDonald <scruffyfurn@hotmail.com>
2018-06-26 13:02:12 -0400

    Added basic example test (TestExamples.js), yarn test to run

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,10 +1,19 @@
-/* global it */
+/* global it, expect */
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import {sum} from './TestExamples' // import Sum from the app test file (Sum is a function)
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(<App />, div)
   ReactDOM.unmountComponentAtNode(div)
+})
+
+// Below is a sample test that calls the SUM function from a
+// imported file , this should be used as a starting point for all tests
+it('A simple example test (sums to numbers)', () => {
+  // Expect is a global function that takes a function to test as a parameter
+  // Expect provides member functions that perform condition checks (for example toEqual,toBeLessThan)
+  expect(sum(1, 1)).toEqual(2)
 })
---
+++ src/components/App/TestExamples.js
@@ -0,0 +1,6 @@
+// Test examples
+
+// Test sum function for export
+export function sum (i, j) {
+  return i + j
+}
