cc7ec682a3e169e854500a147004f927f09e87d8
David Washington <david.washington@microsoft.com>
2018-06-27 10:07:52 -0500

    Add option key to dropdown
    Quiet warnings

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -6,7 +6,7 @@ class NavBar extends React.Component {
     super(props)
     this.title = props.title
     this.state = {value: ''}
-    this.data = ['coconut', 'banana', 'lime']
+    this.data = [{key: 0, value: 'coconut'}, {key: 1, value: 'banana'}, {key: 2, value: 'lime'}]
     this.handleChange = this.handleChange.bind(this)
   }
 
@@ -21,7 +21,7 @@ class NavBar extends React.Component {
         <span>
           <button className='control'>Generators</button>
           <select className='control' value={this.state.value} onChange={this.handleChange}>
-            {this.data.map(x => <option>{x}</option>)}
+            {this.data.map(x => <option key={x.key}>{x.value}</option>)}
           </select>
           <button className='control'>Add Hub</button>
         </span>
